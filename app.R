494c8a7f5a3dc1615729c4a646c9d3b376b5c7a4
AlexandreRives <alexandre.rives85@gmail.com>
2022-01-16 13:16:12 +0100

    Préparation intégration base de données, ajout bouton rafraîchir les données.

--- app.R
+++ app.R
@@ -13,6 +13,8 @@ library(shinycssloaders)
 library(shiny)
 library(DT)
 library(leaflet.extras)
+library(DBI)
+
 
 # Partie front
 ui <- shinyUI(fluidPage(
@@ -20,21 +22,34 @@ ui <- shinyUI(fluidPage(
     # Charger le style
     includeCSS("www/style.css"),
 
-    # Chargement de la page
+    # Page complete
     dashboardPage(
         
         skin = "yellow",
         
+        # Header
         dashboardHeader(title="Job Mining", titleWidth = 300),
+        
         
         dashboardSidebar(width = 300,
             sidebarMenu(
                 menuItem("Resume des offres", tabName = "resume", icon = icon("table")),
                 menuItem("Carte des offres", tabName = "carte", icon = icon("map marked alt")),
                 menuItem("Analyse d'un document", tabName = "offre", icon = icon("file")),
-                menuItem("Analyse d'un corpus", tabName = "corpus", icon = icon("archive"))
+                menuItem("Analyse d'un corpus", tabName = "corpus", icon = icon("archive")),
+                HTML(paste0(
+                    "<br><br><br><br><br><br><br><br><br>",
+                    "<table style='margin-left:auto; margin-right:auto;'>",
+                    "<tr>",
+                    "<p style = 'text-align: center;'><small>LinkedIn :</a></small></p>",
+                    "<p style = 'text-align: center;'><small><a href='https://www.linkedin.com/in/deffa-ndiaye' target='_blank'>Deffa Ndiaye</a></small></p>",
+                    "<p style = 'text-align: center;'><small><a href='https://www.linkedin.com/in/ines-kara' target='_blank'>Ines Kara</a></small></p>",
+                    "<p style = 'text-align: center;'><small><a href='https://www.linkedin.com/in/rives-alexandre/' target='_blank'>Alexandre Rives</a></small></p>",
+                    "</tr>",
+                    "</table>",
+                    "<br>"),
             )
-        ),
+        )),
         
         dashboardBody(
             
@@ -44,10 +59,11 @@ ui <- shinyUI(fluidPage(
                 tabItem(
                     tabName = "resume",
                     # Bouton rafraichir
+                    fluidRow(box(width = 12, title = "Resume des offres")),
                     mainPanel(
-                        fluidRow(actionButton(inputId = "update" ,"Rafraichir", icon("refresh"), class = "btn btn-primary")),
-                        HTML(paste0("<br>")),
-                        fluidRow(DT::datatable(data=iris, filter = 'top', options = list(lengthMenu = c(10, 15, 20), pageLength = 10)))
+                    fluidRow(actionButton(inputId = "update", label = "Rafraichir", icon(name = "refresh", class = "fa-2x"))),
+                    HTML(paste0("<br>")),
+                    DT::datatable(data=mtcars, filter = 'top', options = list(lengthMenu = c(10, 15, 20), pageLength = 10))
                     )
                 ),
                 
@@ -79,12 +95,30 @@ ui <- shinyUI(fluidPage(
 
 # Partie serveur
 server <- shinyServer(function(input, output, session) {
+    
+    
+    # Connection a la base de donnees
+    # output$tbl <- renderTable({
+    #     conn <- dbConnect(
+    #         drv = RMySQL::MySQL(),
+    #         dbname = "shinydemo",
+    #         host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+    #         username = "guest",
+    #         password = "guest")
+    #     on.exit(dbDisconnect(conn), add = TRUE)
+    #     dbGetQuery(conn, paste0(
+    #         "SELECT * FROM City LIMIT ", input$nrows, ";"))
+    # })
+    
+    #Coordonnees
+    Latitude <- c(45.764043, 45.764050)
+    Longitude <- c(4.835659, 4.835870)
 
     # Carte
     output$carte <- renderLeaflet({leaflet() %>%
     addProviderTiles(providers$OpenStreetMap.Mapnik, group = "Open Street Map", options = providerTileOptions(noWrap = TRUE)) %>%
         addFullscreenControl() %>%
-        addMarkers(lat = 45.764043, lng = 4.835659) %>%
+        addMarkers(lat = Latitude, lng = Longitude) %>%
         addLayersControl(
             baseGroups = c("Open Street Map"),
             position = c("topleft"),
